0bc1f1a8c1df2e7b2efb5d5e620e82b18ce3e9dd
Yves Talboom <yves.talboom@gmail.com>
2014-09-28 15:13:50 +0200

    Add readme

---
+++ mongo.js
@@ -0,0 +1,64 @@
+db.productList.insert([{
+    "_id" : ObjectId("5427fb91a217efe9630643b7"),
+    "name" : "Kayak",
+    "description" : "A boat for one person",
+    "category" : "Watersports",
+    "price" : "275"
+},
+    {
+        "_id" : ObjectId("5427fbdca217efe9630643b8"),
+        "name" : "Lifejacket",
+        "description" : "Protective and fashionable",
+        "category" : "Watersports",
+        "price" : "48.95"
+    },
+    {
+        "_id" : ObjectId("5427fc22a217efe9630643b9"),
+        "name" : "Soccer Ball",
+        "description" : "FIFA-approved size and weight",
+        "category" : "Soccer",
+        "price" : "19.9"
+    },
+    {
+        "_id" : ObjectId("5427fc66a217efe9630643ba"),
+        "name" : "Corner flags",
+        "description" : "Give your playing field a professional touch",
+        "category" : "Soccer",
+        "price" : "34.95"
+    },
+    {
+        "_id" : ObjectId("5427fcada217efe9630643bb"),
+        "name" : "Stadium",
+        "description" : "Flat-packed 35,000-seat Stadium",
+        "category" : "Soccer",
+        "price" : "79500.00"
+    },
+    {
+        "_id" : ObjectId("5427fcefa217efe9630643bc"),
+        "name" : "Thinking Cap",
+        "description" : "Improve your brain efficiency by 75%",
+        "category" : "Chess",
+        "price" : "16"
+    },
+    {
+        "_id" : ObjectId("5427fd2fa217efe9630643bd"),
+        "name" : "Unsteady chair",
+        "description" : "Secretly give your opponent a disadvantage",
+        "category" : "Chess",
+        "price" : "29.95"
+    },
+    {
+        "_id" : ObjectId("5427fd69a217efe9630643be"),
+        "name" : "Human Chess Board",
+        "description" : "A fun game for the family",
+        "category" : "Chess",
+        "price" : "75"
+    },
+    {
+        "_id" : ObjectId("5427fda8a217efe9630643bf"),
+        "name" : "Bling-Bling King",
+        "description" : "Gold-plated, diamond-studded King",
+        "category" : "Chess",
+        "price" : "1200"
+    }])
+
---
+++ readme.md
@@ -0,0 +1,33 @@
+#Nodejs & express API server example
+
+##Set up project
+
+Make sure you have installed mongo and node js
+
+Open a terminal and install the following
+
+npm update -g express
+npm update -g generator-express
+
+Clone the project from git. Change the directory to nodetest2 where package.json is and enter the following in the terminal
+* npm install
+
+##Set up the database
+
+* Create a data folder in nodetest2. This is the point where mongo is going to put it's files
+* Open a terminal and start mongo daemon with following command: 
+ulimit -n 2048  & mongod --dbpath ~/IdeaProjects/nodetest2/nodetest2/data
+* Open a terminal and type mongo
+* In the mongo shell type *use nodetest2*
+* To insert the data type the following copy the command in the mongo.js
+
+##Set up intellij
+
+Right MB click in app.js and create a run configuration. Edit this run configuration and point
+the javascript file input field to bin/www
+
+###Run
+
+In intellij hit the green arrow and point your browser to
+
+http://localhost:3000/products/productlist
